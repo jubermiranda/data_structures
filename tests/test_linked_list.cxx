c454884488f991e67d65ece1f6b2f1e22a5f5ee6
uniapp <uniapp@uniapp>
2025-02-01 22:28:35 -0500

    initial test new list: empty on create

--- tests/test_linked_list.cxx
+++ tests/test_linked_list.cxx
@@ -1,54 +1,10 @@
+#include "linked_list.h"
+
 #include <gtest/gtest.h>
-#include <string>
 
-#include "list.h"
+TEST(LinkedListTest, IsEmptyOnCreate) {
+  MY_DS::LinkedList<int> list;
 
-using std::string;
-
-TEST(ListTest, CreateList) {
-  MY_DS::List<int> my_list;
-  EXPECT_EQ(my_list.size(), 0);
-  EXPECT_TRUE(my_list.is_empty());
-}
-
-TEST(ListTest, PushElements) {
-  MY_DS::List<int> my_list;
-
-  // int
-  EXPECT_EQ(my_list.size(), 0);
-  my_list.push(4);
-  my_list.push(2);
-  EXPECT_EQ(my_list.size(), 2);
-  my_list.push(3);
-  my_list.push(1);
-  EXPECT_EQ(my_list.size(), 4);
-
-  // string
-  MY_DS::List<string> my_list_str = MY_DS::List<string>();
-  EXPECT_EQ(my_list_str.size(), 0);
-  my_list_str.push("test0");
-  my_list_str.push("test1");
-  EXPECT_EQ(my_list_str.size(), 2);
-  my_list_str.push("test2");
-  my_list_str.push("test3");
-  EXPECT_EQ(my_list_str.size(), 4);
-}
-
-TEST(ListTest, PopElement) {
-  MY_DS::List<int> my_list;
-  my_list.push(4);
-  my_list.push(2);
-  my_list.push(3);
-  my_list.push(1);
-
-  EXPECT_EQ(my_list.size(), 4);
-  my_list.pop();
-  my_list.pop();
-  EXPECT_EQ(my_list.size(), 2);
-  my_list.pop();
-  my_list.pop();
-  EXPECT_EQ(my_list.size(), 0);
-
-  my_list.pop();
-  EXPECT_EQ(my_list.size(), 0);
+  EXPECT_TRUE(list.is_empty());
+  EXPECT_EQ(list.size(), 0);
 }
