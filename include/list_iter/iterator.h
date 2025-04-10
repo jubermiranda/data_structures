c1ebfe6e35c34d7e4a05a5f6cf726f5fb74533bd
uniapp <uniapp@uniapp>
2025-04-10 07:40:21 -0400

    add const_iterator

--- include/list_iter/iterator.h
+++ include/list_iter/iterator.h
@@ -25,40 +25,98 @@ public:
     valid_crr_node();
     return &(this->crr->data);
   }
-  iterator<T>& operator++() {
+  iterator<T> &operator++() {
     valid_crr_node();
     this->crr = this->crr->next;
     return *this;
   }
-  iterator<T>& operator--() {
+  iterator<T> &operator--() {
     valid_prev_node();
     this->crr = this->crr->prev;
     return *this;
   }
-  iterator<T>& operator++(int) {
+  iterator<T> &operator++(int) {
     iterator<T> initial = *this;
     this->operator++();
     return initial;
   }
-  iterator<T>& operator--(int) {
+  iterator<T> &operator--(int) {
     iterator<T> initial = *this;
     this->operator--();
     return initial;
   }
-  bool operator==(const iterator<T>& other) const {
+  bool operator==(const iterator<T> &other) const {
     return this->crr == other.crr;
   }
-  bool operator!=(const iterator<T>& other) const {
+  bool operator!=(const iterator<T> &other) const {
     return !(this->operator==(other));
   }
 
 private:
-  void valid_crr_node() const{
-    if(this->crr == nullptr)
+  void valid_crr_node() const {
+    if (this->crr == nullptr)
       throw std::invalid_argument("End of list");
   }
   void valid_prev_node() const {
-    if(this->crr != nullptr && this->crr->prev != nullptr)
+    if (this->crr != nullptr && this->crr->prev != nullptr)
+      return;
+    throw std::invalid_argument("Invalid previous node");
+  }
+};
+
+template <typename T> class const_iterator {
+  friend class MY_DS::List<T>;
+
+private:
+  List<T> *src;
+  DNode<T> *crr;
+
+  const_iterator(List<T> *src_list, DNode<T> *pos) : src(src_list), crr(pos) {}
+
+public:
+  const T &operator*() const {
+    valid_crr_node();
+    return this->crr->data;
+  }
+  const T *operator->() const {
+    valid_crr_node();
+    return &(this->crr->data);
+  }
+  const_iterator<T> &operator++() {
+    valid_crr_node();
+    this->crr = this->crr->next;
+    return *this;
+  }
+  const_iterator<T> &operator--() {
+    valid_prev_node();
+    this->crr = this->crr->prev;
+    return *this;
+  }
+  const_iterator<T> &operator++(int) {
+    const_iterator<T> initial = *this;
+    this->operator++();
+    return initial;
+  }
+  const_iterator<T> &operator--(int) {
+    const_iterator<T> initial = *this;
+    this->operator--();
+    return initial;
+  }
+
+  bool operator==(const_iterator<T> &other) const {
+    return this->crr == other.crr;
+  }
+  bool operator!=(const_iterator<T> &other) const {
+    return !(this->operator==(other));
+  }
+
+private:
+  void valid_crr_node() const {
+    if (this->crr == nullptr)
+      throw std::invalid_argument("End of list");
+  }
+  void valid_prev_node() const {
+    if (this->crr != nullptr && this->crr->prev != nullptr)
       return;
     throw std::invalid_argument("Invalid previous node");
   }
