e83460c27d841292478cfc029092faa6cf146a61
uniapp <uniapp@uniapp>
2025-04-10 01:53:44 -0400

    adjust iterator and write list interface

--- include/list_iter/iterator.h
+++ include/list_iter/iterator.h
@@ -1,5 +1,4 @@
-#ifndef ITERATOR_H_
-#define ITERATOR_H_
+#pragma once
 
 #include <stdexcept>
 
@@ -13,18 +12,56 @@ template <typename T> class iterator {
 
 private:
   List<T> *src;
-  typename List<T>::DNode *crr;
+  DNode<T> *crr;
 
   iterator(List<T> *src_list, DNode<T> *pos) : src(src_list), crr(pos) {}
 
 public:
   T &operator*() const {
-    if (this->crr == nullptr)
-      throw std::invalid_argument("End of list");
+    valid_crr_node();
     return this->crr->data;
+  }
+  T *operator->() const {
+    valid_crr_node();
+    return &(this->crr->data);
+  }
+  iterator<T>& operator++() {
+    valid_crr_node();
+    this->crr = this->crr->next;
+    return *this;
+  }
+  iterator<T>& operator--() {
+    valid_prev_node();
+    this->crr = this->crr->prev;
+    return *this;
+  }
+  iterator<T>& operator++(int) {
+    iterator<T> initial = *this;
+    this->operator++();
+    return initial;
+  }
+  iterator<T>& operator--(int) {
+    iterator<T> initial = *this;
+    this->operator--();
+    return initial;
+  }
+  bool operator==(const iterator<T>& other) const {
+    return this->crr == other.crr;
+  }
+  bool operator!=(const iterator<T>& other) const {
+    return !(this->operator==(other));
+  }
+
+private:
+  void valid_crr_node() const{
+    if(this->crr == nullptr)
+      throw std::invalid_argument("End of list");
+  }
+  void valid_prev_node() const {
+    if(this->crr != nullptr && this->crr->prev != nullptr)
+      return;
+    throw std::invalid_argument("Invalid previous node");
   }
 };
 
 }; // namespace MY_DS
-
-#endif // ITERATOR_H_
--- include/list_iter/list.h
+++ include/list_iter/list.h
@@ -1,5 +1,4 @@
-#ifndef ITER_LIST_H_
-#define ITER_LIST_H_
+#pragma once
 
 #include <cstddef>
 #include "iterator.h"
@@ -13,27 +12,47 @@ public:
   friend class iterator<T>;
 
 private:
-  DNode<T> *begin;
-  DNode<T> *end;
+  DNode<T> *head;
+  DNode<T> *tail;
   size_t num_items;
 
 public:
   List();
   List(const List<T> &other);
-  ~List();
   List<T> &operator=(const List<T> &other);
+  ~List();
 
   void push_front(const T &item);
   void push_back(const T &item);
   void pop_front();
   void pop_back();
-
   iterator<T> insert(iterator<T> pos, const T &item);
   iterator<T> erase(iterator<T> pos);
+
+  T& front();
+  const T& front() const;
+  T& back();
+  const T& back() const;
+  iterator<T> begin();
+  const iterator<T> begin() const;
+  iterator<T> end();
+  const iterator<T> end() const;
+
+  void swap(List<T> other);
+  bool is_empty() const;
+  size_t size() const;
+
+  void clear();
+
+  bool operator==(const List<T>&) const ;
+  bool operator==(void *) const ;
+  bool operator!=(const List<T>&) const ;
+  bool operator!=(void *) const ;
+
 };
+
+#include "list_iter/list.tpp"
 
 } // namespace MY_DS
 
-#include "list.tpp"
 
-#endif // ITER_LIST_H_
