242cad8f66ab4ddead1b2555b12b3b1605c5a5ff
uniapp <uniapp@uniapp>
2025-06-06 20:57:41 -0400

    test(b_tree): test << operator
    - test stream b_tree (test fails)

--- tests/test_b_tree.cxx
+++ tests/test_b_tree.cxx
@@ -19,3 +19,19 @@ TEST(BinaryTreeTest, InsertAndSize) {
   tree.insert(2);
   EXPECT_EQ(tree.size(), 2);
 }
+
+TEST(BinaryTreeTest, PrintBtreeAfterInserts) {
+  b_tree<int> tree;
+  tree.insert(4);
+  tree.insert(2);
+  tree.insert(6);
+  tree.insert(1);
+  tree.insert(3);
+  tree.insert(5);
+  tree.insert(7);
+
+  std::ostringstream oss;
+  oss << tree;
+
+  EXPECT_EQ(oss.str(), "4\n2\n1\n3\n6\n5\n7\n");
+}
