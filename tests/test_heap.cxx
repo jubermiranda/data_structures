705354790ac2f70b966e17560ce025886ac1676c
Juber <>
2025-08-01 14:11:22 -0400

    test: check size after insert
    - (test fails)

--- tests/test_heap.cxx
+++ tests/test_heap.cxx
@@ -7,3 +7,11 @@ TEST(MaxHeapTree, CheckSizeAfterCreation) {
   MY_DS::heap<int> heap;
   ASSERT_EQ(heap.size(), 0);
 }
+
+TEST(MaxHeapTree, CheckSizeAfterInsertion) {
+  MY_DS::heap<int> heap;
+  heap.insert(10);
+  ASSERT_EQ(heap.size(), 1);
+  heap.insert(20);
+  ASSERT_EQ(heap.size(), 2);
+}
